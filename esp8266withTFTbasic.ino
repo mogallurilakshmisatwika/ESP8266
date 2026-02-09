aa645d6dd4b05bfc33d835d977d61fb776535e58
mogallurilakshmisatwika <mogallurilakshmisatwika@gmail.com>
2026-02-09 17:55:50 +0530

    Add files via upload
    ESP8266 with 2.4 TFT SPI

--- esp8266withTFTbasic.ino
+++ esp8266withTFTbasic.ino
@@ -1,23 +1,39 @@
 #include <SPI.h>
-#include <SD.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
 
-#define SD_CS D2
+#define TFT_CS   D8
+#define TFT_DC   D4
+#define TFT_RST  D3
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 
 void setup() {
-  Serial.begin(9600);
-  delay(3000);
-
-  Serial.println();
-  Serial.println("ESP8266 SD RAW TEST");
-
-  SPI.begin();   // force SPI start
-
-  if (!SD.begin(SD_CS)) {
-    Serial.println("SD INIT FAILED");
-    return;
-  }
-
-  Serial.println("SD INIT SUCCESS");
+  tft.begin();
+  tft.setRotation(1);   // landscape
 }
 
-void loop() {}
+void loop() {
+
+  tft.fillScreen(ILI9341_RED);
+  delay(1000);
+
+  tft.fillScreen(ILI9341_GREEN);
+  delay(1000);
+
+  tft.fillScreen(ILI9341_BLUE);
+  delay(1000);
+
+  tft.fillScreen(ILI9341_BLACK);
+
+  // Draw shapes
+  tft.drawRect(20, 20, 100, 60, ILI9341_YELLOW);
+  tft.fillCircle(200, 120, 30, ILI9341_CYAN);
+
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(2);
+  tft.setCursor(60, 200);
+  tft.println("Graphics OK");
+
+  delay(3000);
+}
